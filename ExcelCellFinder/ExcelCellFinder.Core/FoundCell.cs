0d18bb241da9a000d9da4f6561f18c431bc1fefe
Hayato Ouchi <hayato_ouchi@yayoi-kk.co.jp>
2024-08-02 21:49:41 +0900

    エクセルセル検索ツールをとりあえず作りました (#1)
    * EXCELセル検索ツール作成
    
    * とりあえず動くように改良

---
+++ ExcelCellFinder/ExcelCellFinder.Core/FoundCell.cs
@@ -0,0 +1,24 @@
+﻿using ClosedXML.Excel;
+
+namespace ExcelCellFinder.Core
+{
+    public class FoundCell(string wbPath, IXLCell cell, Exception? e)
+    {
+        public string WorkbookPath { get; } = wbPath;
+
+        public string SheetName { get; } = cell.Worksheet.Name;
+
+        public int RowNumber { get; } = cell.Address.RowNumber;
+
+        public int ColumnNumber { get; } = cell.Address.ColumnNumber;
+
+        public string Column { get; } = cell.Address.ColumnLetter;
+
+        public string? ErrorMessage { get; } = e?.Message;
+
+        public override string ToString()
+        {
+            return $"{SheetName}!{Column}{RowNumber}";
+        }
+    }
+}
