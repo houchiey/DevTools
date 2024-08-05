22fb4a88da4313c64e087262cdfdaf585582c3d4
勇人 大内 <tbh.kyomei0705@gmail.com>
2024-08-05 23:15:21 +0900

    プロジェクトのひな型、ウインドウ・ページを作成。デザインテーマにModernWpfを当てた。

---
+++ ExcelCellFinder/ExcelCellFinder.App/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExcelCellFinder.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Page SetupConditionsPage { get; set; }
+        public Page ResultPage { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetupConditionsPage = new SetupConditionsPage();
+            ResultPage = new ResultPage();
+
+            Content.Navigate(SetupConditionsPage);
+        }
+    }
+}
